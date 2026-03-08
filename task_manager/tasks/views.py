e415e8144cf76d126021ab0c0891b405ddaee498
Mamun <beparimamun708@gmail.com>
2026-03-08 12:04:39 +0600

    Complete Authentication System: signup, login, logout

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+
+
+def signup_view(request):
+   if request.method == 'POST':
+      form = SignUpForm(request.POST)
+      if form.is_valid():
+         user = form.save()
+         login(request, user)
+         return redirect('login')
+   else: 
+      form = SignUpForm()
+
+   return render(request, 'tasks/signup.html', {'form': form})
+
+def login_view(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+      if form.is_valid():
+         user = form.get_user()
+         login(request, user)
+         return redirect('login')
+   else:
+      form = AuthenticationForm()
+   return render(request, 'tasks/login.html', {'form': form })
+
+def logout_view(request):
+   if request.method == 'POST':
+      logout(request)
+      return redirect('login')
+   
+   
