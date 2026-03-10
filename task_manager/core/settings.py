8aec5d297562685625a75479db0dadbe276583f8
Mamun <beparimamun708@gmail.com>
2026-03-10 11:22:15 +0600

    Step 5: Completed PostgreSQL Setup

--- task_manager/core/settings.py
+++ task_manager/core/settings.py
@@ -1,5 +1,10 @@
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-767l#ujt*f^1d6!wlyo*1++sj_xwvx^h5o=4y&8q(fh4(bn=$z'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -63,12 +69,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.getenv('DB_NAME'),
+      'USER': os.getenv('DB_USER'),
+      'PASSWORD': os.getenv('DB_PASSWORD'),
+      'HOST': os.getenv('DB_HOST'),
+      'PORT': os.getenv('DB_PORT'),
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
