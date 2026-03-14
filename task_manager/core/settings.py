930f101d2f1c54f069ccd4b78e7f64ea7eafb8ed
Mamun <beparimamun708@gmail.com>
2026-03-14 19:36:54 +0600

    Final setup: procfile, .env, settings.py

--- task_manager/core/settings.py
+++ task_manager/core/settings.py
@@ -21,8 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['task-manager-app-l3r1.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 
 # Application definition
